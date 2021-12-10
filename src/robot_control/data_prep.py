d33ffc2a057f43dffff860b13f4b3b0e644ca442
Owen Brooks <owen.h.brooks@gmail.com>
2021-12-10 14:08:37 +1100

    Add transition data preparation script (unused for now)

---
+++ src/robot_control/data_prep.py
@@ -0,0 +1,43 @@
+# Manipulate recorded data
+# Instead of gripper_state being binary for open (True) and close (False),
+# It will store one of three values: do_nothing (0), trigger_open (1), trigger_close (2)
+
+import argparse
+from enum import IntEnum
+import os
+import pandas as pd
+
+class GripperAction(IntEnum):
+    DoNothing=0
+    TriggerOpen=1
+    TriggerClose=2
+
+def main(args):
+    session_id = args.session
+    current_dirname = os.path.dirname(__file__)
+    data_dir = os.path.join(current_dirname, '../../data')
+    annotations_file = os.path.join(data_dir, session_id, session_id + '.csv')
+    df = pd.read_csv(annotations_file, sep=' ')
+
+    action_list = []
+    for i in range(len(df)-1):
+        curr_frame_open = df.iloc[i]['gripper_is_open']
+        next_frame_open = df.iloc[i+1]['gripper_is_open']
+        if curr_frame_open and not next_frame_open:
+            action = GripperAction.TriggerClose
+        elif not curr_frame_open and next_frame_open:
+            action = GripperAction.TriggerOpen
+        else:
+            action = GripperAction.DoNothing
+        action_list.append(int(action))
+    # print(action_list)
+
+    df = pd.Series(action_list)
+    print(df.value_counts())
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--session', default='2021-12-09-04:56:05', type=str)
+    args = parser.parse_args()
+    main(args)
