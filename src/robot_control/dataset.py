6da7eef05fab85651c3e8a9a5134a2ff9a41add6
Owen Brooks <owen.h.brooks@gmail.com>
2021-12-16 14:08:51 +1100

    Load data to ram

--- src/robot_control/dataset.py
+++ src/robot_control/dataset.py
@@ -21,43 +21,40 @@ class DeepHandoverDataset(Dataset):
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
             ])
+        
+        # Load entire dataset into RAM
+        self.image_tensors = []
+        self.force_tensors = []
+        self.gripper_state_tensors = []
+        for idx in range(self.img_labels.shape[0]):
+            img_path = os.path.join(data_dir, self.session_id, 'images', self.img_labels.iloc[idx, 0])
+            image = Image.open(img_path)
+            label = self.img_labels.iloc[idx, 1]
+
+            image_tensor = self.transform(image)
+
+            force_tensor = torch.Tensor([
+                self.img_labels["fx"][idx],
+                self.img_labels["fy"][idx],
+                self.img_labels["fz"][idx],
+                self.img_labels["mx"][idx],
+                self.img_labels["my"][idx],
+                self.img_labels["mz"][idx]
+            ])
+
+            gripper_state_tensor = torch.Tensor([
+                self.img_labels["gripper_is_open"][idx]
+            ])
+
+            self.image_tensors.append(image_tensor)
+            self.force_tensors.append(force_tensor)
+            self.gripper_state_tensors.append(gripper_state_tensor)
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        current_dirname = os.path.dirname(__file__)
-        data_dir = os.path.join(current_dirname, '../../data')
-        img_path = os.path.join(data_dir, self.session_id, 'images', self.img_labels.iloc[idx, 0])
-        image = Image.open(img_path)
-        label = self.img_labels.iloc[idx, 1]
-
-        image_tensor = self.transform(image)
-
-        force_tensor = torch.Tensor([
-            self.img_labels["fx"][idx],
-            self.img_labels["fy"][idx],
-            self.img_labels["fz"][idx],
-            self.img_labels["mx"][idx],
-            self.img_labels["my"][idx],
-            self.img_labels["mz"][idx]
-        ])
-
-        gripper_state_tensor = torch.Tensor([
-            self.img_labels["gripper_is_open"][idx]
-        ])
-
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-
-        sample = {}
-        sample['image'] = image_tensor
-        sample['force'] = force_tensor
-        sample['gripper_is_open'] = gripper_state_tensor
-
-        return image_tensor, force_tensor, gripper_state_tensor
+        return self.image_tensors[idx], self.force_tensors[idx], self.gripper_state_tensors[idx]
 
 if __name__ == "__main__":
     data = DeepHandoverDataset("2021-12-01-15:30:36")
