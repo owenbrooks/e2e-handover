c3351142567a774993769b4f5040f2c30a3f9923
Owen Brooks <owen.h.brooks@gmail.com>
2021-12-17 16:45:21 +1100

    Add better ergonomics for instance seg demo

--- src/robot_control/segmentation.py
+++ src/robot_control/segmentation.py
@@ -2,27 +2,32 @@ import cv2
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import Visualizer, ColorMode
 from detectron2.config import get_cfg
+from detectron2.projects import point_rend
+from detectron2.data import MetadataCatalog
 
-def inference():
+def inference(im):
     cfg = get_cfg()
+    # Add PointRend-specific config
+    point_rend.add_pointrend_config(cfg)
+    # Load a config from file
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-    # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8  # set the testing threshold for this model
     predictor = DefaultPredictor(cfg)
-    im = cv2.imread("input.jpg")
     outputs = predictor(im)
-    print(outputs['instances'].pred_masks.shape)
-    print(outputs['instances'].pred_classes.detach().cpu().numpy())
-    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+    coco_metadata = MetadataCatalog.get("coco_2017_val")
+
+    v = Visualizer(im[:, :, ::-1], coco_metadata, scale=1.2, instance_mode=ColorMode.IMAGE_BW)
+    point_rend_result = v.draw_instance_predictions(outputs["instances"].to("cpu")).get_image()
+
     while True:
-        cv2.imshow('out', out.get_image()[:, :, ::-1])
+        cv2.imshow('res', point_rend_result[:, :, ::-1])
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 if __name__ == "__main__":
-    inference()
+    im = cv2.imread("2760_2021-12-14-23:19:09.png")
+    inference(im)
