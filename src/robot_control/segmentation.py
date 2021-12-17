e3b637af41e66c4ec2d398bb61d40519877ec007
Owen Brooks <owen.h.brooks@gmail.com>
2021-12-17 16:57:18 +1100

    Extract segmentor into a class for reuse

--- src/robot_control/segmentation.py
+++ src/robot_control/segmentation.py
@@ -7,25 +7,31 @@ from detectron2.config import get_cfg
 from detectron2.projects import point_rend
 from detectron2.data import MetadataCatalog
 
-def inference(im):
-    cfg = get_cfg()
-    point_rend.add_pointrend_config(cfg) # Add PointRend-specific config
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
-    coco_metadata = MetadataCatalog.get("coco_2017_val")
+class Segmentor():
+    def __init__(self):
+        cfg = get_cfg()
+        point_rend.add_pointrend_config(cfg) # Add PointRend-specific config
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
 
-    predictor = DefaultPredictor(cfg)
-    outputs = predictor(im)
+        self.predictor = DefaultPredictor(cfg)
+        self.metadata = MetadataCatalog.get("coco_2017_val")
 
-    v = Visualizer(im[:, :, ::-1], coco_metadata, scale=1.2, instance_mode=ColorMode.IMAGE_BW)
-    point_rend_result = v.draw_instance_predictions(outputs["instances"].to("cpu")).get_image()
+    def inference(self, im):
+        outputs = self.predictor(im)
 
-    while True:
-        cv2.imshow('res', point_rend_result[:, :, ::-1])
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        v = Visualizer(im[:, :, ::-1], self.metadata, scale=1.2, instance_mode=ColorMode.IMAGE_BW)
+        point_rend_result = v.draw_instance_predictions(outputs["instances"].to("cpu")).get_image()
+
+        return point_rend_result[:, :, ::-1]
 
 if __name__ == "__main__":
-    im = cv2.imread("2760_2021-12-14-23:19:09.png")
-    inference(im)
+    im = cv2.imread("input.png")
+    seg = Segmentor()
+    point_rend_result = seg.inference(im)
+
+    while True:
+        cv2.imshow('res', point_rend_result)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
