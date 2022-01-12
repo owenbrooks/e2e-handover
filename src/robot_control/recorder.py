20d7daf374ec8a6ed098023a05be18e8e2a00114
Owen Brooks <owen.h.brooks@gmail.com>
2022-01-13 10:13:08 +1100

    Fix data directory

--- src/robot_control/recorder.py
+++ src/robot_control/recorder.py
@@ -26,7 +26,7 @@ class Recorder():
 
         # Create folder for storing recorded images and the csv with numerical/annotation data
         current_dirname = os.path.dirname(__file__)
-        data_dir = os.path.join(current_dirname, '../data')
+        data_dir = os.path.join(current_dirname, '../../data')
         data_dir_exists = os.path.exists(data_dir)
         if not data_dir_exists:
             os.makedirs(data_dir)
@@ -40,11 +40,11 @@ class Recorder():
             current_dirname = os.path.dirname(__file__)
             image_name = str(self.session_image_count) + '_' + self.session_id + '.png'
             self.session_image_count += 1
-            image_path = os.path.join(current_dirname, '../data', self.session_id, 'images', image_name)
+            image_path = os.path.join(current_dirname, '../../data', self.session_id, 'images', image_name)
             cv2.imwrite(image_path, image)
 
             # Append numerical data and annotation to the session csv
-            csv_path = os.path.join(current_dirname, '../data', self.session_id, self.session_id + '.csv')
+            csv_path = os.path.join(current_dirname, '../../data', self.session_id, self.session_id + '.csv')
             with open(csv_path, 'a+') as csvfile:
                 datawriter = csv.writer(csvfile, delimiter=' ')
                 datawriter.writerow([image_name, gripper_is_open] + wrench.tolist() + self.twist_array)
@@ -58,7 +58,7 @@ class Recorder():
             # Create folder for storing recorded images and the session csv
             self.session_id = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
             current_dirname = os.path.dirname(__file__)
-            session_dir = os.path.join(current_dirname, '../data', self.session_id)
+            session_dir = os.path.join(current_dirname, '../../data', self.session_id)
             image_dir = os.path.join(session_dir, 'images')
             os.makedirs(image_dir)
             # Create csv file for recording numerical data and annotation in the current session
