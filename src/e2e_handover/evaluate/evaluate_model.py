64a9f9c3ac74bce789bf8863f072921ceb32a8ff
Owen Brooks <owen.h.brooks@gmail.com>
2022-02-07 15:10:01 +1100

    Fix model evaluation with new model

--- src/e2e_handover/evaluate/evaluate_model.py
+++ src/e2e_handover/evaluate/evaluate_model.py
@@ -2,10 +2,11 @@ import argparse
 from collections import namedtuple
 from datetime import datetime
 from e2e_handover.train.dataset import DeepHandoverDataset
-from e2e_handover.train.model import MultiViewResNet
+from e2e_handover.train.model_double import MultiViewResNet
 import json
 import numpy as np
 import os
+import sys
 import torch
 from torch.utils.data import random_split 
 import yaml
@@ -13,11 +14,9 @@ import yaml
 """ This file performs inference using a given model on a dataset and calculates
     statistics giving an idea of the model's accuracy. """
 
-def main(session_id, model_name, test_fraction):
+def main(params, model_path):
     # Create network and load weights
-    net = MultiViewResNet()
-    current_dirname = os.path.dirname(__file__)
-    model_path = os.path.join(current_dirname, '../../../models', model_name)
+    net = MultiViewResNet(params)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     net.load_state_dict(torch.load(model_path, map_location=device))
     print("Using device: " + str(device))
@@ -25,8 +24,8 @@ def main(session_id, model_name, test_fraction):
     net.eval()
 
     # Load dataset
-    dataset = DeepHandoverDataset(session_id)
-    train_length = int(len(dataset)*(1.0-test_fraction))
+    dataset = DeepHandoverDataset(params)
+    train_length = int(len(dataset)*(1.0-params.test_fraction))
     test_length = len(dataset) - train_length
     _, test_data = random_split(dataset, [train_length, test_length], generator=torch.Generator().manual_seed(42))
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=True, num_workers=4, pin_memory=True)
@@ -37,12 +36,23 @@ def main(session_id, model_name, test_fraction):
     # Perform inference on all test images
     with torch.no_grad():
         for i, data in enumerate(test_loader, 0):
-            img = torch.Tensor(data[0]).to(device)
-            forces = torch.Tensor(data[1]).to(device)
-            ground_truth = torch.Tensor(data[2]).to(device)
+            # get the inputs
+            input_img = torch.Tensor(data['image']).to(device)
+            input_forces = torch.Tensor(data['force']).to(device)
+            target_gripstate = torch.Tensor(data['gripper_is_open']).to(device)
 
-            model_open[i] = net(img, forces).cpu().data.numpy() > 0.5
-            ground_truth_open[i] = ground_truth.cpu().data.numpy()
+            if params.use_lstm:
+                target_gripstate = target_gripstate[:, 4, :].unsqueeze(1) # extract final gripstate from sequence
+
+            if params.output_velocity:
+                target_vel_cmd = torch.Tensor(data['vel_cmd']).to(device)
+
+            # forward + backward + optimize
+            outputs = net(input_img, input_forces)
+            pred_gripstate = outputs[:, 0].unsqueeze(1)
+
+            model_open[i] = pred_gripstate.cpu().data.numpy() > 0.5
+            ground_truth_open[i] = target_gripstate.cpu().data.numpy()
 
             print(f"{i+1}/{test_length} complete")
 
@@ -75,10 +85,10 @@ def main(session_id, model_name, test_fraction):
 
     timestamp = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
     stats = {
-        'model_name': model_name, 
-        'data_session_id': session_id,
+        'model_name': os.path.split(model_path)[-1], 
+        'data': params.data_file,
         'test_timestamp': timestamp ,
-        'test_fraction': test_fraction,
+        'test_fraction': params.test_fraction,
         'using_segmentation': os.environ.get('use_segmentation') is not None,
         'accuracy': accuracy,
         'f_score': f_score
@@ -89,7 +99,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser('Evaluate model on dataset')
     parser.add_argument('--data', type=str, help='path of csv file to train on e.g. data/2021-12-09-04:56:05/raw.csv')
-    parser.add_argument('--model-name', type=str, required=True, help='model file stored in top level models/ e.g. 2021-12-14-23.pt')
+    parser.add_argument('--model', type=str, required=True, help='model file stored in top level models/ e.g. 2021-12-14-23.pt')
 
     current_dirname = os.path.dirname(__file__)
     params_path = os.path.join(current_dirname, 'params.yaml')
@@ -104,7 +114,7 @@ if __name__ == "__main__":
         params['data_file'] = args.data
 
         params = namedtuple("Params", params.keys())(*params.values())
-        main(params, args.model_name)
+        main(params, args.model)
     # example:
-    # python3 evaluate_model.py --model-name 2021-12-14-23.pt --data data/2021-12-14-23/2021-12-14-23_calib_short.csv
+    # python3 evaluate_model.py --model models/2021-12-14-23.pt --data data/2021-12-14-23/2021-12-14-23_calib_short.csv
     # model must be in models directory specified in params.yaml
